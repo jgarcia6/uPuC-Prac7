06c299d086245508dc4ccac724d70ae46f899a71
JG <garcia.jesus@uabc.edu.mx>
2021-04-15 17:07:42 -0700

    Updating to mini-game logic

--- Prac7_IO.c
+++ Prac7_IO.c
@@ -4,39 +4,84 @@
 #define SetBitPort(port, bit) __asm__ ( ?? )
 #define ClrBitPort(port, bit) __asm__ ( ?? )
 
-enum ButtonPressStates
-{
-  eBtnUndefined = 0,
-  eBtnShortPressed,
-  eBtnLongPressed
-}
-// External definitions (ASM)
-extern void delay(uint16_t mseg);
+#define TIME_WINDOW 300
+#define SEED xx
 
-// Local definitions prototypes
+enum ButtonStates
+{
+    eBtnUndefined = 0,
+    eBtnShortPressed,
+    eBtnLongPressed
+}
+
+enum GameStates
+{
+    eGameRestart = 0,
+    eWaitForStart
+    eStartCount,
+    eEndCount,
+    eGameOver,
+    eYouWin,
+    eYouLoose
+}
+
+// Prototypes
+extern uint8_t myRand(uint8_t seed);
+extern void    delay(uint16_t mseg);
 void    InitPorts(void);
 uint8_t checkBtn(void);
-void    updateLeds(void);
+void    updateLeds(uint8_t gameState);
 
-// Global variables
-uint8_t  globalCounter=0;
+// Global variable
 uint32_t millis;
 
 int main(void)
 {
-	InitPorts();
+    uint8_t  currentGameState = eGameRestart;
+    uint16_t countdown = 0;
+    uint16_t countup = 0;
+    InitPorts();
 
-	while(1)
-    {
-		switch(check_Btn())
+    while(1)
+    {    
+        switch(check_Btn())
         {
-			case eBtnShortPressed: globalCounter++;
-						break;
-			case eBtnLongPressed:	globalCounter--;
-						break;
-		}
-		updateLeds();
-		delay(1);
-		millis++;	
-	}
+            case eBtnShortPressed: currentGameState++;
+                break;
+            case eBtnLongPressed:  currentGameState = eGameRestart;
+                break;
+        }
+        
+        switch(currentGameState)
+        {
+            case eGameRestart:
+                countdown = (myRand(SEED)+1)*20;
+                countup = 0;
+                currentGameState++;
+                break;
+            case eWaitForStart:
+                break;
+            case eStartCount:
+                countdown--;
+                if (countdown == 0)
+                currentGameState++;
+                break;
+            case eEndCount:
+                if (countdown != 0)
+                currentGameState++;
+                else
+                countup++;
+                break;
+            case eGameOver:
+                if ((countdown+countup)>TIME_WINDOW)
+                currentGameState = eYouLoose;
+                else 
+                currentGameState = eYouWin;
+                break;
+        }
+        
+        updateLeds(currentGameState);
+        delay(1);
+        millis++;
+    }
 }
